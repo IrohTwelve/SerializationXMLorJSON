81f69283df5aba13861614178357b6735376b49e
IrohTwelve <osoleksandr@gmail.com>
2017-07-22 08:55:34 +0300

    Changed  group class

--- src/main/java/com/osoleksandr/Shapes/Group.java
+++ src/main/java/com/osoleksandr/Shapes/Group.java
@@ -1,5 +1,22 @@
 package com.osoleksandr.Shapes;
 
-public class Group {
+import com.osoleksandr.Conventor.XmlTags;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Group extends Shape {
+
+    XmlTags tags;
+    List<Shape> groupOfShapes = new ArrayList<Shape>();
+
+    public String toXML() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(tags.getGroupOpenTag());
+        for (int i = 0; i < groupOfShapes.size(); i++) {
+            sb.append(groupOfShapes.get(i).toXML());
+        }
+        sb.append(tags.getGroupCloseTag());
+        return sb.toString();
+    }
 }
